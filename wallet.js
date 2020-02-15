0935495cb11aa126c0cb7356b07177fbde5485af
Scylin232 <timyr3@ukr.net>
2020-02-15 16:30:47 +0300

    Screenshot

--- wallet.js
+++ wallet.js
@@ -20,6 +20,7 @@ app.get('/', async (req, res) => {
   await page.click('button[class="button relative"]');
   console.log('Clicked Login twice and screenshot time out')
   // await page.waitForNavigation({ waitUntil: 'networkidle0' });
+  await page.screenshot({ path: 'timeout.png' });
   console.log('Redirected')
   await page.setRequestInterception(true);
   console.log('Start capturing packages')
