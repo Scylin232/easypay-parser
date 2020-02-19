9c008ead8f8cedbbe40791b9d48b29e268914f91
Scylin232 <timyr3@ukr.net>
2020-02-19 14:55:25 +0300

    Recreated EasyPay System

--- wallet.js
+++ wallet.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const puppeteer = require('puppeteer');
 const express = require('express');
 const cors = require('cors');
@@ -5,15 +6,15 @@ const schedule = require('node-schedule');
 
 const appPort = process.env.PORT || 4515
 const app = express();
-let returnData = [];
+let easyPayParams = {};
 
 app.use(cors());
 
 let credentials = { login: '380992077402', password: 'Lolik232' };
 
-schedule.scheduleJob('*/15 * * * *', async () => {
-  returnData = [];
-  const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
+const getToken = async () => {
+  const tempArray = [];
+  const browser = await puppeteer.launch({headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.setUserAgent('5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
   await page.goto('https://easypay.ua/ua', { waitUntil: 'networkidle0' });
@@ -25,24 +26,54 @@ schedule.scheduleJob('*/15 * * * *', async () => {
   await page.setRequestInterception(true);
   page.on('request', req => {
     if (req._headers.authorization) {
-      returnData.push(req._headers);
+      tempArray.push(req._headers);
       req.abort();
     } else {
       req.continue();
     };
   });
   await new Promise(r => setTimeout(r, 10000));
+  easyPayParams = { pageId: tempArray[0].pageid, appId: tempArray[0].appid, bearerToken: tempArray[0].authorization.split(' ')[1] };
   await browser.close();
   console.log('All completed succesfuly.');
+};
+
+schedule.scheduleJob('*/15 * * * *', async () => {
+  await getToken();
 });
 
-app.get('/', async (req, res) => {
-  if (returnData[0] === undefined) {
-    return console.log('Data is undefined');
-  };
-  console.log('Returned: ', credentials);
-  res.send({ pageId: returnData[0].pageid, appId: returnData[0].appid, bearerToken: returnData[0].authorization.split(' ')[1] });
-});
+app.get('/wallets', async (req, res) => {
+  if (easyPayParams.pageId === undefined) {
+    return await res.status(200).send('Token is unvailable.');
+  }
+  const easypay = await axios({
+    url: 'https://api.easypay.ua/api/wallets/get',
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${easyPayParams.bearerToken}`,
+      'AppId': easyPayParams.appId,
+      'PageId': easyPayParams.pageId
+    }
+  })
+  return await res.status(200).send(easypay.data.wallets);
+})
+
+app.get('/getWalletById', async (req, res) => {
+  if (easyPayParams.pageId === undefined || req.query.walletId === undefined) {
+    return await res.status(200).send('Token is unvailable.');
+  }
+  const walletId = req.query.walletId;
+  const easypay = await axios({
+    url: `https://api.easypay.ua/api/wallets/get/${walletId}`,
+    method: 'GET',
+    headers: {
+      'Authorization': `Bearer ${easyPayParams.bearerToken}`,
+      'AppId': easyPayParams.appId,
+      'PageId': easyPayParams.pageId
+    }
+  });
+  return await res.status(200).send(easypay.data.wallets);
+})
 
 app.post('/setCredentials', async (req, res) => {
   const { login, password } = req.query;
@@ -53,5 +84,9 @@ app.post('/setCredentials', async (req, res) => {
 });
 
 app.listen(appPort, () => {
-  console.log('Example app listening on port: ', appPort);
+  console.log('App listening on port: ', appPort);
 });
+
+(async () => {
+  await getToken();
+})();
