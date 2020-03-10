8d81593530cb6b128624eaadc9b4c924157b9748
Scylin232 <timyr3@ukr.net>
2020-03-11 00:50:11 +0300

    Removed Puppeteer

--- wallet.js
+++ wallet.js
@@ -1,4 +1,3 @@
-const puppeteer = require('puppeteer');
 const axios = require('axios');
 const sha1 = require('sha1');
 const schedule = require('node-schedule');
@@ -6,32 +5,54 @@ const fs = require('fs');
 
 const easyPayGetToken = async () => {
   try {
-    const tempArray = [];
-    const [login, password] = await fs.readFileSync('../easypayCredentials.dat', 'utf-8').split(/\r?\n/);
-    const browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
-    await page.setUserAgent('5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36');
-    await page.goto('https://easypay.ua/ua', { waitUntil: 'networkidle0' });
-    await page.click('button[class="header__sign-in shrink medium-5 column"]');
-    await page.type('input[id="sign-in-phone"]', login);
-    await page.type('input[id="password"]', password);
-    await page.click('button[class="button relative"]');
-    await page.waitForNavigation({ waitUntil: 'networkidle0' });
-    await page.setRequestInterception(true);
-    page.on('request', req => {
-      if (req._headers.authorization) {
-        tempArray.push(req._headers);
-        req.abort();
-      } else {
-        req.continue();
-      };
+    const [login, password] = await fs.readFileSync('../easypayCredentials.dat', 'utf-8').split(/\r?\n/);    
+    const easypayData = await axios({
+      url: 'https://api.easypay.ua/api/system/createApp',
+      method: 'POST',
+      headers: {
+        'authority': 'api.easypay.ua',
+        'content-length': '0',
+        'appid': 'null',
+        'locale': 'ru',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+        'content-type': 'application/json; charset=UTF-8',
+        'accept': 'application/json, text/plain, */*',
+        'sec-fetch-dest': 'empty',
+        'pageid': 'null',
+        'partnerkey': 'easypay-v2',
+        'origin': 'https://easypay.ua',
+        'sec-fetch-site': 'same-site',
+        'sec-fetch-mode': 'cors',
+        'referer': 'https://easypay.ua/',
+        'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+      }
     });
-    await new Promise(r => setTimeout(r, 10000));
-    await fs.writeFileSync('../easypayData.dat', `${tempArray[0].pageid}\n${tempArray[0].appid}\n${tempArray[0].authorization.split(' ')[1]}`);
-    await browser.close();
+    const easypayToken = await axios({
+      url: 'https://api.easypay.ua/api/token',
+      method: 'POST',
+      headers: {
+        'authority': 'api.easypay.ua',
+        'content-length': '0',
+        'appid': `${easypayData.data.appId}`,
+        'locale': 'ru',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36',
+        'content-type': 'application/json; charset=UTF-8',
+        'accept': 'application/json, text/plain, */*',
+        'sec-fetch-dest': 'empty',
+        'pageid': `${easypayData.data.pageId}`,
+        'partnerkey': 'easypay-v2',
+        'origin': 'https://easypay.ua',
+        'sec-fetch-site': 'same-site',
+        'sec-fetch-mode': 'cors',
+        'referer': 'https://easypay.ua/',
+        'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+      },
+      data:`grant_type=password&username=${login}&password=${password}&client_id=easypay-v2`,
+    });
+    fs.writeFileSync('../easypayData.dat', `${easypayData.data.pageId}\n${easypayData.data.appId}\n${easypayToken.data.access_token}`);
     console.log('EasyPay, at:', new Date().toLocaleString('ru-RU', {timeZone: 'Europe/Kiev'}));
   } catch(err) {
-    console.log(err.message);
+    console.log(err.response.data);
   };
 };
 
@@ -47,7 +68,7 @@ const globalMoneyGetToken = async () => {
   };
 };
 
-schedule.scheduleJob('*/15 * * * *', async () => {
+schedule.scheduleJob('*/20 * * * *', async () => {
   await easyPayGetToken();
 });
 
